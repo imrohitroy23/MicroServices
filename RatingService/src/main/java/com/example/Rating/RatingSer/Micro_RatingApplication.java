e8f46743ad819733223db588ce33b30d7dd043df
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-02-03 00:24:19 +0530

    commit

--- RatingService/src/main/java/com/example/Rating/RatingSer/Micro_RatingApplication.java
+++ RatingService/src/main/java/com/example/Rating/RatingSer/Micro_RatingApplication.java
@@ -2,8 +2,10 @@ package com.example.Rating.RatingSer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Micro_RatingApplication {
 
 	public static void main(String[] args) {
